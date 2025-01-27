45c94fb5f7dff39c31674ec918089d8eac4e8ad2
Ilya <illyaetoilysha@gmail.com>
2025-01-27 18:35:53 +0000

    Added User saving to database

--- app/Http/Telegram/Commands/StartCommand.php
+++ app/Http/Telegram/Commands/StartCommand.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Telegram\Commands;
 
+use App\Models\User;
 use DefStudio\Telegraph\Handlers\WebhookHandler;
 
 class StartCommand extends WebhookHandler
 {
     public function start(): void
     {
+        User::query()->firstOrCreate([
+            'chat_id' => $this->message->from()?->id(),
+            'username' => $this->message->from()?->username(),
+            'first_name' => $this->message->from()?->firstName(),
+            'last_name' => $this->message->from()?->lastName(),
+        ]);
+
         $first_name = $this->message->from()?->firstName();
 
         $this->chat->html(
